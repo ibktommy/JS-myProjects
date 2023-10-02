80dc3ef3c4f21bdd3b78d8fa35d5e1332e26d391
ibktommy <atomquartz@gmail.com>
2023-10-02 08:07:00 +0100

    hour-countdown-project structure with styles

---
+++ hour-countdown/app.js
@@ -0,0 +1,8 @@
+const hoursValue = Number(document.querySelector('.hours p').textContent);
+const minutesValue = Number(document.querySelector('.minutes p').textContent);
+const secondsValue = Number(document.querySelector('.seconds p').textContent);
+
+console.log(hoursValue);
+console.log(minutesValue);
+console.log(secondsValue);
+
