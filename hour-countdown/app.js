f7084605d20d1cf2b34593648e61a3b24d1f6c08
ibktommy <atomquartz@gmail.com>
2023-10-05 07:30:47 +0100

    Created a secondsCountDown that gets called each seconds by a setInterval-callback-function

--- hour-countdown/app.js
+++ hour-countdown/app.js
@@ -1,8 +1,26 @@
-const hoursValue = Number(document.querySelector('.hours p').textContent);
-const minutesValue = Number(document.querySelector('.minutes p').textContent);
-const secondsValue = Number(document.querySelector('.seconds p').textContent);
+const hoursValue = document.querySelector('.hours p');
+const minutesValue = document.querySelector('.minutes p');
+const secondsValue = document.querySelector('.seconds p');
 
-console.log(hoursValue);
-console.log(minutesValue);
-console.log(secondsValue);
+// console.log(hoursValue);
+// console.log(minutesValue);
+console.log(secondsValue.textContent);
+
+let startSecondsCountDown;
+
+
+// Function to Coundown seconds
+function secondsCountDown() {
+  let secondsNumber = Number(secondsValue.textContent)
+
+  secondsNumber = secondsNumber - 1
+
+  secondsValue.textContent = secondsNumber.toString()  
+
+  if (secondsValue.textContent === '0') {
+    clearInterval(startSecondsCountDown)
+  } 
+}
+
+startSecondsCountDown = setInterval(secondsCountDown, 1000);
 
