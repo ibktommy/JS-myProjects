254cb1f026a15492a5f41b4bab88b83076c768c4
ibktommy <atomquartz@gmail.com>
2023-10-05 13:12:13 +0100

    Created the hoursCountdown-function, reset the minutesValues and secondsValue immediately it is called, setConditions in the seconds CountDown that calls the minutesCountDown or the houseCountDown

--- hour-countdown/app.js
+++ hour-countdown/app.js
@@ -6,11 +6,18 @@ const secondsValue = document.querySelector('.seconds p');
 // console.log(minutesValue);
 console.log(secondsValue.textContent);
 
-let startSecondsCountDown;
-let startMinutesCountDown;
+let startCountDown;
 
 // Function to Coundown seconds
 function secondsCountDown() {
+  if (secondsValue.textContent === '00' && minutesValue.textContent === '00') {
+    hoursCountDown()
+  }
+
+  if (secondsValue.textContent === "00" && minutesValue.textContent !== '00') {
+    minutesCountDown()
+  }
+
 	let secondsNumber = Number(secondsValue.textContent);
 
 	secondsNumber = secondsNumber - 1;
@@ -20,19 +27,11 @@ function secondsCountDown() {
 	} else if (secondsNumber <= 9) {
 		secondsValue.innerHTML = '0' + secondsNumber.toString();
 	}
-
-	if (secondsValue.textContent === '00') {
-		// Wait for a seconds so as to show the "00" value of seconds before calling the minutesCountDown-function
-		setTimeout(() => {
-			minutesCountDown();
-		}, 1000);
-	}
 }
-startSecondsCountDown = setInterval(secondsCountDown, 1000);
 
 // Function to Countdown minutes
 function minutesCountDown() {
-	secondsValue.innerHTML = '10'; //Reset the value of seconds
+	secondsValue.innerHTML = '60'; //Reset the value of seconds
 
 	let minutesNumber = Number(minutesValue.textContent);
 
@@ -44,3 +43,22 @@ function minutesCountDown() {
 		minutesValue.innerHTML = '0' + minutesNumber.toString();
 	}
 }
+
+// Function to Coundown Hours
+function hoursCountDown() {
+  minutesValue.innerHTML = '59'; //Reset the value of minutes
+  secondsValue.innerHTML = '60'; //Reset the value of seconds
+
+  let hoursNumber = Number(hoursValue.textContent);
+
+  hoursNumber= hoursNumber- 1
+
+  if (hoursNumber > 9) {
+		hoursValue.innerHTML = hoursNumber.toString();
+	} else if (hoursNumber <= 9) {
+		hoursValue.innerHTML = '0' + hoursNumber.toString();
+	}
+}
+
+
+startCountDown = setInterval(secondsCountDown, 1000);
